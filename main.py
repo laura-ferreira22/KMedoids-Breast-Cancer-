6706e6ebb3c80a166f2fcaee3c462936d28e9d58
Laura Ferreira de Almeida <laura.ferreira@ufba.br>
2025-02-22 11:11:11 -0300

    melhor k por wcss e gráfico cotovelo

--- main.py
+++ main.py
@@ -27,3 +27,28 @@ plt.title("Dados Originais")
 plt.xlabel(breast_cancer.feature_names[0])
 plt.ylabel(breast_cancer.feature_names[8])
 plt.show()
+
+# Função para calcular WCSS
+def calcular_wcss(X, k_max):
+  wcss=[]
+  for k in range (1, k_max + 1):
+    kmedoids = KMedoids(n_clusters = k, random_state = 42)
+    kmedoids.fit(X)
+    wcss.append(kmedoids.inertia_)
+  return wcss
+
+# Definir o número máximo de clusters para testar
+k_max = 10
+
+# Calcular WCSS para diferentes valores de k
+wcss = calcular_wcss(X, k_max)
+
+
+# Plotar o gráfico de cotovelo
+plt.figure(figsize=(8,6))
+plt.plot(range(1, k_max+1), wcss, marker ='o', linestyle = '--')
+plt.title("Método Elbow para determinar o número ideal de Clusters")
+plt.xlabel("Número de clusters (k)")
+plt.ylabel("Soma dos quadrados dentr do cluster (wcss)")
+plt.grid(True)
+plt.show()
