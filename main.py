a8f5dc88506f539632722a14eec9d41424091f7c
Laura Ferreira de Almeida <laura.ferreira@ufba.br>
2025-02-22 11:07:27 -0300

    visualização do dataset pra pré-processamento

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ from sklearn_extra.cluster import KMedoids
 
 # Carregar o dataset Breast Cancer
 breast_cancer = load_breast_cancer()
+
+# Exibir características
+breast_cancer.feature_names
+
+#Exibir rótulos
+breast_cancer.target_names
+
+#Escolhe duas características do dataset, colunas 0 e 8 ('mean radius' e 'mean symmetry')
+X = breast_cancer.data[:,[0,8]]
+
+#Exibir os dados (primeiras 5 linhas)
+X[:5]
